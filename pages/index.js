eb6cba6d8750ec06fdc9db4483f8d194452a8fd6
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2024-02-29 15:41:03 +0700

    feat: Tugas hari ke - 9

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Main() {
+	useEffect(() => {
+		fetch("/api/hello")
+			.then((res) => res.json())
+			.then((res) => console.log("responsr => ", res))
+			.catch((err) => console.log("error => ", err));
+	}, []);
 	return (
 		<Layout metaTitle={"Home"} metaDescription={"ini adalah home"}>
 			<div className="text-red">Home</div>
