4ad63dd4e56fcc449fa893a1f3fcbec308d095d0
RenaufalAlrafid <renaufalalrafid@mail.ugm.ac.id>
2024-03-01 11:22:08 +0700

    feat: Tugas Hari 10

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Layout from "@/layout";
 import { useEffect } from "react";
+import dynamic from "next/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const Layout = dynamic(() => import("@/layout"));
 
 export default function Main() {
 	useEffect(() => {
@@ -14,7 +16,16 @@ export default function Main() {
 	}, []);
 	return (
 		<Layout metaTitle={"Home"} metaDescription={"ini adalah home"}>
-			<div className="text-red">Home</div>
+			<div className="text-red bg-white">
+				Home
+				{/* <Image src="/next.png" alt="Next.js Logo" width={400} height={400} />
+				<img
+					src="/next.png"
+					style={{
+						width: 400,
+						height: 400,
+					}}></img> */}
+			</div>
 		</Layout>
 	);
 }
